afd18ca6aedff2aa6460492d9c00cfd3f12c6f01
Kapshaul <kapshaul@gmail.com>
2025-02-06 16:20:29 -0800

    Update PPO

--- PPO/model.py
+++ PPO/model.py
@@ -86,24 +86,28 @@ class PPONetwork_CNN(nn.Module):
 
         self.conv1 = nn.Sequential(
             # [N, 32, 20, 20]
-            nn.Conv2d(4, 64, kernel_size=8, stride=4, bias=False),
-            nn.BatchNorm2d(num_features=64, eps=1e-5, momentum=0.1),
-            nn.AvgPool2d(2, 2),
-            #nn.ReLU(),
+            nn.Conv2d(4, 32, kernel_size=8, stride=4, bias=True),
+            #nn.BatchNorm2d(num_features=64, eps=1e-5, momentum=0.1),
+            #nn.AvgPool2d(2, 2),
+            nn.ReLU(),
         )
 
         self.conv2 = nn.Sequential(
             # [N, 64, 9, 9]
-            nn.Conv2d(64, 16, kernel_size=3, stride=1, bias=False),
-            nn.BatchNorm2d(num_features=16, eps=1e-5, momentum=0.1),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, bias=True),
             nn.ReLU(),
         )
-        #self.pool = nn.AvgPool2d(kernel_size, stride)
+
+        self.conv3 = nn.Sequential(
+            # [N, 64, 9, 9]
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=True),
+            nn.ReLU(),
+        )
 
         # Common layer
         self.common = nn.Sequential(
             #nn.Linear(9*9*64, hidden_size),
-            CustomLinear(8*8*16, hidden_size, 0.9),
+            CustomLinear(7*7*64, hidden_size, 0.9),
             FeatureScaler(hidden_size),
             nn.Sigmoid(),
         )
@@ -130,6 +134,7 @@ class PPONetwork_CNN(nn.Module):
         x = x.view(-1, 4, 84, 84)
         x = self.conv1(x)
         x = self.conv2(x)
+        x = self.conv3(x)
         x = x.flatten(start_dim=1)
 
         features = self.common(x)
